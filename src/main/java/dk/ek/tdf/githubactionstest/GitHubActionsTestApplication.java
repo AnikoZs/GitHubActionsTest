467d5f5176b8fa2d64c9055740cddccfb1fe51cf
Aniko Zsigri <ANIZ@ek.dk>
2026-09-15 15:10:32 +0200

    Initial commit

---
+++ src/main/java/dk/ek/tdf/githubactionstest/GitHubActionsTestApplication.java
@@ -0,0 +1,13 @@
+package dk.ek.tdf.githubactionstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubActionsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/ek/tdf/githubactionstest/GitHubActionsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.ek.tdf.githubactionstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActionsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
